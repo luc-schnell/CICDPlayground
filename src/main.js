9d5dc589d0ae7b4e410f31e0bff47f3ea0f37789
Luc Schnell <luc.schnell@tngtech.com>
2025-05-16 13:50:03 +0200

    Modify main.js file.

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "James Butler",
+    name: "Luc Schnell",
     // In the following fiels you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
-    associations: ["Ehm, what?"],
+    associations: ["fail fast and improve from there"],
     // Which CI/CD tools do you use in your project?
-    tools: "Jenkins, maybe?",
+    tools: "GitHub Actions, GitLab CI/CD",
     // What do you want to learn in this workshop?
-    expectations: ["what you are talking about"],
+    expectations: ["I would like to learn common CI/CD frameworks and techniques"],
   },
 });
